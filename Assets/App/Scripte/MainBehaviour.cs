eb7d73b7e42f74b788168c39aca13daf1c6ec481
drumath <2237k.drumath@gmail.com>
2024-12-18 04:45:13 +0900

    :sparkles: awaitable impl

--- Assets/App/Scripte/MainBehaviour.cs
+++ Assets/App/Scripte/MainBehaviour.cs
@@ -1,9 +1,30 @@
+using System;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class MainBehaviour : MonoBehaviour
 {
-    private void Start()
+    [SerializeField]
+    private TextMeshProUGUI logText;
+
+    private async void Start()
     {
-        Debug.Log("Hello!");
+        try
+        {
+            logText.text = await GetStringAsync(5);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    private static async Awaitable<string> GetStringAsync(int delay, CancellationToken token = default)
+    {
+        token.ThrowIfCancellationRequested();
+
+        await Awaitable.WaitForSecondsAsync(delay, token);
+        return "Hello!";
     }
 }
